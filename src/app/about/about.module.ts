00d34f7dcfb644afc37ac843cc11de0019a124aa
TobiasHBrandt <39334625+TobiasHBrandt@users.noreply.github.com>
2025-01-03 17:33:48 +0100

    Lazy Routes

--- src/app/about/about.module.ts
+++ src/app/about/about.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { AboutComponent } from './about.component';
+import { RouterModule } from '@angular/router';
+
+const ABOUT_ROUTES = [
+  {
+    path: '',
+    component: AboutComponent,
+  }
+]
 
 @NgModule({
+  imports: [
+    RouterModule.forChild(ABOUT_ROUTES)
+  ],
   declarations: [AboutComponent],
   exports: [AboutComponent]
 })
