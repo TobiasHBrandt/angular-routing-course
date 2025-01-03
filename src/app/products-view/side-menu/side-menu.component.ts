95649ed4d57cbcb087e6dfe2af0a2fc2a1e56cea
TobiasHBrandt <39334625+TobiasHBrandt@users.noreply.github.com>
2025-01-03 16:13:25 +0100

    nested and Auxiliary Routes

--- src/app/products-view/side-menu/side-menu.component.ts
+++ src/app/products-view/side-menu/side-menu.component.ts
@@ -3,6 +3,7 @@ import { PieService } from '../../services/pie.service';
 import { AsyncPipe, NgFor, NgIf } from '@angular/common';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { tap } from 'rxjs';
+import { PRODUCT_ROUTER_TOKENS } from '../products.routes';
 
 @Component({
   standalone: true,
@@ -12,6 +13,7 @@ import { tap } from 'rxjs';
     AsyncPipe,
     RouterLink,
     RouterLinkActive,
+    RouterLinkActive,
   ],
   selector: 'app-side-menu',
   templateUrl: './side-menu.component.html',
@@ -20,6 +22,8 @@ import { tap } from 'rxjs';
 export class SideMenuComponent {
   @Input() customize = false;
   pies$ = this.pieService.filteredPies$;
+  readonly customizeLink = `./${PRODUCT_ROUTER_TOKENS.CUSTOMIZE}`;
+  readonly detailLink = `./${PRODUCT_ROUTER_TOKENS.DETAIL}`;
 
   constructor(
     private readonly pieService: PieService,
